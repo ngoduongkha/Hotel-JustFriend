0cc94f5233f4b52bcbbcf7c8565719b544dc17b5
Ngô Dương Kha <57293030+ngoduongkha@users.noreply.github.com>
2021-03-08 16:30:34 +0700

    update

---
+++ Hotel JustFriend/Hotel JustFriend/Models/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_JustFriend.Models
+{
+    class DataProvider
+    {
+        private static DataProvider instance;
+
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return instance;
+            }
+            set { instance = value; }
+        }
+
+        public Hotel_JustFriendEntities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new Hotel_JustFriendEntities();
+        }
+    }
+}
