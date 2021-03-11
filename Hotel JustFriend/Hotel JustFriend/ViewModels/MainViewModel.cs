8184e51988a9a07c9b883b7632ea7bbd20063768
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-03-11 09:58:05 +0700

    menu slider

--- Hotel JustFriend/Hotel JustFriend/ViewModels/MainViewModel.cs
+++ Hotel JustFriend/Hotel JustFriend/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using Hotel_JustFriend.Models;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Hotel_JustFriend.ViewModels
@@ -13,6 +14,8 @@ namespace Hotel_JustFriend.ViewModels
         public ICommand MaximizeWindowCommand { get; set; }
         public ICommand MinimizeWindowCommand { get; set; }
         public ICommand MouseMoveWindowCommand { get; set; }
+        public ICommand MenuOpenCommand { get; set; }
+        public ICommand MenuCloseCommand { get; set; }
         #endregion
         public MainViewModel()
         {
@@ -20,8 +23,18 @@ namespace Hotel_JustFriend.ViewModels
             MaximizeWindowCommand = new RelayCommand<object>(p => true, p => MaximizeWindow(p));
             MinimizeWindowCommand = new RelayCommand<object>(p => true, p => MinimizeWindow(p));
             MouseMoveWindowCommand = new RelayCommand<object>(p => true, p => MouseMoveWindow(p));
+            MenuOpenCommand = new RelayCommand<object>(p => true, p => MenuOpen(p));
+            MenuCloseCommand = new RelayCommand<object>(p => true, p => MenuClose(p));
         }
         #region Methods
+        public void MenuOpen(object p)
+        {
+            if (p != null) (p as Border).IsEnabled = false;
+        }
+        public void MenuClose(object p)
+        {
+            if (p != null) (p as Border).IsEnabled = true;
+        }
         public void MouseMoveWindow(object p)
         {
             if (p != null)
