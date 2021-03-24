8afe831759c653d67e11a1ffcdbb2d3602c5b15c
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-03-24 15:46:59 +0700

    account

--- Hotel JustFriend/Hotel JustFriend/Views/AccountManageView.xaml.cs
+++ Hotel JustFriend/Hotel JustFriend/Views/AccountManageView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hotel_JustFriend.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Hotel_JustFriend.Views
         public AccountManageView()
         {
             InitializeComponent();
+            this.DataContext = new AccountManageViewModel();
         }
     }
 }
