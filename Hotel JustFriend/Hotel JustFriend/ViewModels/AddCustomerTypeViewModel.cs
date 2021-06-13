1307749da246134708d970b63c8b21370980842a
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-06-13 13:06:10 +0700

    kkk

---
+++ Hotel JustFriend/Hotel JustFriend/ViewModels/AddCustomerTypeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hotel_JustFriend.ViewModels
+{
+    [POCOViewModel]
+    class AddCustomerTypeViewModel : ViewModelBase
+    {
+        [Command]
+        public void MouseMoveWindow(Window p)
+        {
+            try
+            {
+                p.DragMove();
+            }
+            catch { return; }
+        }
+
+        [Command]
+        public void CloseWindow(Window p)
+        {
+            try
+            {
+                p.Close();
+            }
+            catch { return; }
+        }
+    }
+}
