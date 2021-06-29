1b09e7bfd60867f4295680797a8d0bb1b3564e14
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-06-29 19:45:31 +0700

    mabedone

--- Hotel JustFriend/Hotel JustFriend/ViewModels/ReportViewModel.cs
+++ Hotel JustFriend/Hotel JustFriend/ViewModels/ReportViewModel.cs
@@ -17,62 +17,69 @@ namespace Hotel_JustFriend.ViewModels
 {
     class ReportViewModel : ViewModelBase
     {
-        private ObservableCollection<TypeRoom> _ListRoomType;
-        private ObservableCollection<Room> _ListRoom;
-        private ObservableCollection<Bill> _ListBill;
-        private ObservableCollection<BillInfo> _ListBillInfo;
-        private string _month;
-        private string _year;
+        private ObservableCollection<TypeRoom> listRoomType;
+        private ObservableCollection<Room> listRoom;
+        private ObservableCollection<Bill> listBill;
+        private ObservableCollection<BillInfo> listBillInfo;
+        private ObservableCollection<int> listYears;
         private SeriesCollection pieSerieCollection;
 
-        public string Year { get => _year; set => _year = value; }
-        public string Month { get => _month; set => _month = value; }
         public SeriesCollection PieSerieCollection { get => pieSerieCollection; set { pieSerieCollection = value; RaisePropertiesChanged(); } }
+        public ObservableCollection<int> ListYears { get => listYears; set => listYears = value; }
+
+        public ReportViewModel()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            listRoomType = new ObservableCollection<TypeRoom>(DataProvider.Instance.DB.TypeRooms.Where(p => p.IsDelete == false));
+            listBillInfo = new ObservableCollection<BillInfo>(DataProvider.Instance.DB.BillInfoes);
+            listRoom = new ObservableCollection<Room>(DataProvider.Instance.DB.Rooms);
+            ListYears = new ObservableCollection<int>(DataProvider.Instance.DB.Bills.Select(x => x.Date.Year).Distinct());
+        }
 
         [Command]
-        public void report(Grid grid)
+        public void ShowReport(ReportView uc)
         {
-            ReportTemplateUC aa = new ReportTemplateUC();
-            aa.month.Text = Month.ToString();
-            aa.year.Text = Year.ToString();
-            decimal tt = 0;
-            _ListRoomType = new ObservableCollection<TypeRoom>(DataProvider.Instance.DB.TypeRooms.Where(p => p.IsDelete == false));
-            _ListBillInfo = new ObservableCollection<BillInfo>(DataProvider.Instance.DB.BillInfoes);
-            _ListRoom = new ObservableCollection<Room>(DataProvider.Instance.DB.Rooms);
-            int y = int.Parse(Year);
-            int m = 0;
-            if (Month != "") m = int.Parse(Month);
-            if (Month == "") _ListBill = new ObservableCollection<Bill>(DataProvider.Instance.DB.Bills.Where(p => p.Date.Year == y));
-            else _ListBill = new ObservableCollection<Bill>(DataProvider.Instance.DB.Bills.Where(p => p.Date.Year == y && p.Date.Month == m));
-            if (_ListBill.Count > 0)
+            ReportTemplateUC reportTemplate = new ReportTemplateUC();
+            reportTemplate.month.Text = uc.cbboxMonths.Text;
+            reportTemplate.year.Text = uc.cbboxYears.Text;
+            decimal total = 0;
+            int year = int.Parse(uc.cbboxYears.Text);
+            int month = uc.cbboxMonths.SelectedIndex + 1;
+            if (month == 0) listBill = new ObservableCollection<Bill>(DataProvider.Instance.DB.Bills.Where(p => p.Date.Year == year));
+            else listBill = new ObservableCollection<Bill>(DataProvider.Instance.DB.Bills.Where(p => p.Date.Year == year && p.Date.Month == month));
+            if (listBill.Count > 0)
             {
                 PieSerieCollection = new SeriesCollection();
-                for (int i = 0; i < _ListRoomType.Count; i++)
+                for (int i = 0; i < listRoomType.Count; i++)
                 {
-                    double phantram = 0;
-                    decimal doanhthu = 0;
-                    int idtype = _ListRoomType[i].IdTypeRoom;
-                    for (int j = 0; j < _ListBillInfo.Count; j++)
+                    double percent = 0;
+                    decimal revenue = 0;
+                    int idtype = listRoomType[i].IdTypeRoom;
+                    for (int j = 0; j < listBillInfo.Count; j++)
                     {
-                        int idd = _ListBillInfo[j].IdBill;
-                        Bill k = _ListBill.Where(p => p.IdBill == idd).FirstOrDefault();
+                        int idd = listBillInfo[j].IdBill;
+                        Bill k = listBill.Where(p => p.IdBill == idd).FirstOrDefault();
                         if (k != null)
                         {
-                            Room a = _ListRoom.Where(p => p.IdRoom == _ListBillInfo[j].IdRoom).FirstOrDefault();
+                            Room a = listRoom.Where(p => p.IdRoom == listBillInfo[j].IdRoom).FirstOrDefault();
                             if (a.IdTypeRoom == idtype)
                             {
-                                phantram = phantram + 1;
-                                doanhthu = doanhthu + (decimal)k.TotalMoney;
+                                percent = percent + 1;
+                                revenue = revenue + (decimal)k.TotalMoney;
                             }
                         }
                     }
-                    if (_ListBill != null) phantram = phantram / (_ListBill.Count);
-                    tt = tt + doanhthu;
+                    if (listBill != null) percent = percent / (listBill.Count);
+                    total = total + revenue;
                     ChartValues<decimal> chartValue = new ChartValues<decimal>();
-                    chartValue.Add(doanhthu);
+                    chartValue.Add(revenue);
                     PieSeries newPie = new PieSeries
                     {
-                        Title = _ListRoomType[i].DisplayName,
+                        Title = listRoomType[i].DisplayName,
                         Values = chartValue,
                         DataLabels = true,
                         FontSize = 15,
@@ -80,19 +87,36 @@ namespace Hotel_JustFriend.ViewModels
                     PieSerieCollection.Add(newPie);
                     ReportUC c = new ReportUC();
                     c.STT.Text = (i + 1).ToString();
-                    c.displayname.Text = _ListRoomType[i].DisplayName;
-                    c.revenue.Text = string.Format("{0:C}", doanhthu);
-                    c.percent.Text = phantram.ToString("#0.##%");
+                    c.displayname.Text = listRoomType[i].DisplayName;
+                    c.revenue.Text = string.Format("{0:C}", revenue);
+                    c.percent.Text = percent.ToString("#0.##%");
 
-                    aa.stp.Children.Add(c);
+                    reportTemplate.stp.Children.Add(c);
                 }
-                aa.totalmoney.Text = string.Format("{0:C}", tt);
-                grid.Children.Clear();
-                grid.Children.Add(aa);
+                reportTemplate.totalmoney.Text = string.Format("{0:C}", total);
+                uc.gridReportTemplate.Children.Clear();
+                uc.gridReportTemplate.Children.Add(reportTemplate);
             }
             else { MyMessageBox.Show("Dữ liệu không tồn tại, mời nhập lại thời gian", "Nhắc nhở", MessageBoxButton.OK); }
         }
 
+        [Command]
+        public void Export(ReportTemplateUC uc)
+        {
+            try
+            {
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    uc.btnExport.Visibility = Visibility.Hidden;
+                    printDialog.PrintVisual(uc.gridExport, "Bill");
+                }
+            }
+            finally
+            {
+                uc.btnExport.Visibility = Visibility.Visible;
+            }
+        }
     }
 
 }
